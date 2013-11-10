3f0f87042c576ecece9a9cffd3fb4b6c90e1ac63
Jasper <slytherin9090@gmail>
2013-11-10 08:16:23 +0800

    added app.js

--- cyborg/js/app.js
+++ cyborg/js/app.js
@@ -164,4 +164,49 @@
 		function basename(path) {
 			return path.replace(/\\/g,'/').replace( /.*\//, '' );
 		}
+		
+		
+		
+		
 	});
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	function downloadFile(url){
+
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, 
+    function onFileSystemSuccess(fileSystem) {
+        fileSystem.root.getFile(
+        "dummy.html", {create: true, exclusive: false}, 
+        function gotFileEntry(fileEntry) {
+            var sPath = fileEntry.fullPath.replace("dummy.html","");
+            var fileTransfer = new FileTransfer();
+            fileEntry.remove();
+
+            fileTransfer.download(
+               url,
+                sPath + basename(url),
+                function(theFile) {
+                    console.log("download complete: " + theFile.toURI());
+                    showLink(theFile.toURI());
+                },
+                function(error) {
+                    console.log("download error source " + error.source);
+                    console.log("download error target " + error.target);
+                    console.log("upload error code: " + error.code);
+                }
+            );
+        }, fail);
+    }, fail);
+}
+function fail(error){
+	console.log(error);
+}
